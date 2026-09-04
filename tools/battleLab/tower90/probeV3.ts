9363d978b53b15b766009f482ba0c836aead1957
o8054283080-boop <o8054283080@gmail.com>
2026-09-04 15:29:52 +0900

    Fix 90F V3 probe type inference

--- tools/battleLab/tower90/probeV3.ts
+++ tools/battleLab/tower90/probeV3.ts
@@ -35,7 +35,7 @@ export function tower90ProbeV3(context: Context): ScenarioProbe {
   let warDrumBuffUses = 0;
   let warDrumTempoUses = 0;
   let fangKills = 0;
-  let playersAliveBeforeTurn = PLAYERS.length;
+  let playersAliveBeforeTurn: number = PLAYERS.length;
   const deathTurn: Record<string, number> = { E2: 0, E3: 0, E4: 0, E5: 0 };
   let previousAlive = new Set<string>(ESCORTS.filter((id) => context.aliveOf(id)));
 
