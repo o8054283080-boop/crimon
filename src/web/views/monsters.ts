0e93401a99d6dd1b51b016124e9d90315b8a0f0e
o8054283080-boop <o8054283080@gmail.com>
2026-09-12 14:35:57 +0900

    avoid crowded monster header actions

--- src/web/views/monsters.ts
+++ src/web/views/monsters.ts
@@ -123,11 +123,13 @@ function renderList(props: MonstersProps): HTMLElement {
        * 目覚素材の交換所を一度ショップへ置こうとして間違えている
        * (集める場所から離すと使われない)。
        */
-      el("button", { type: "button", className: "btn btn--ghost head-action", onclick: props.onGoStorage }, ["📦 保管所"]),
-      el("button", { type: "button", className: "btn btn--ghost head-action", onclick: props.onGoExchange }, ["🔁 交換所"]),
       el("button", { type: "button", className: "btn btn--ghost head-action", onclick: props.onGoMonsterDex }, ["📖 図鑑"]),
     ]),
     el("section", { className: "panel monsters-list-panel" }, [
+      el("div", { className: "bulk-bar__row" }, [
+        el("button", { type: "button", className: "btn btn--ghost", onclick: props.onGoStorage }, ["📦 モンスター保管所"]),
+        el("button", { type: "button", className: "btn btn--ghost", onclick: props.onGoExchange }, ["🔁 モンスター交換所"]),
+      ]),
       renderMonsterFilterBar({
         all: props.player.monsters,
         shownCount: shown.length,
